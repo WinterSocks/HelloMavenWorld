247d5e8935d3847c4ebf1777392be235954ff1e7
yokoyamaGit <goodsmile2013@mail.ru>
2021-07-23 00:39:39 +0400

    3 times Intellij sucks

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Intellij sucks ");
+
+        System.out.println("Intellij sucks 1");
+        System.out.println("Intellij sucks 2");
+        System.out.println("Intellij sucks 3");
+
     }
 }
