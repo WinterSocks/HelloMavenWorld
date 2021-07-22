04be8609875d87deff0b328f1c04beb3d8a401d9
yokoyamaGit <goodsmile2013@mail.ru>
2021-07-23 00:38:04 +0400

    I added a comment that Intellij looks like a bitch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Intellij sucks ");
+        System.out.println("Intellij sucks as dirty bi*ch");
         System.out.println("Hello");
     }
 }
