416d2c23f1bff6a84f77957d1e5a56e90eb83a00
Yokoyama <goodsmile2013@mail.ru>
2024-02-28 20:05:12 +0200

    message1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Intellij sucks as dirty bi*ch");
         System.out.println("Hello");
+
+        System.out.println("add1");
     }
 }
